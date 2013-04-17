75ad2f8f66424f59197f17eaba012ec9fb54a8b1
Paul Hiatt <hiattp@gmail.com>
2013-04-17 17:14:43 -0400

    continued auth

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({ username: username }, function (err, user) {
       if (err) return done(err);
-      if (!user) return done(null, false, { message: 'Incorrect username.' });
+      if (!user) return done(null, false, { message: "Sorry, we don't recognize that username." });
       user.validPassword(password, function(err, isMatch){
         if(err) return done(err);
         if(isMatch) return done(null, user);
